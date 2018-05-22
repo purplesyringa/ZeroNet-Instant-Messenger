92caaeffd3ca11215793f16b3cb1882c966170fc
Ivanq <imachug@yandex.ru>
2018-05-22 13:38:47 +0300

    Add v-app

--- main/src/vue_components/root.vue
+++ main/src/vue_components/root.vue
@@ -1,7 +1,9 @@
 <template>
-	<div class="current-view">
-		<component :is="$parent.currentView" ref="currentView"></component>
-	</div>
+	<v-app>
+		<div class="current-view">
+			<component :is="$parent.currentView" ref="currentView"></component>
+		</div>
+	</v-app>
 </template>
 
 <script language="text/javascript">

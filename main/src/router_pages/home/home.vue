2f966c05ac47f2460314707e3a20eaa6cb0f40a9
Christian Seibold <krixano@protonmail.com>
2018-05-22 07:09:25 -0500

    Interface upgrades

--- main/src/router_pages/home/home.vue
+++ main/src/router_pages/home/home.vue
@@ -1,52 +1,61 @@
 <template>
-	<v-navigation-drawer v-model="drawer" class="pb-0" floating hide-overlay stateless width="380">
-		<v-layout fill-height>
-      <v-navigation-drawer
-        dark
-        mini-variant
-        stateless
-        value="true"
-      >
-        <v-toolbar flat class="transparent">
-          <v-list class="pa-0">
-            <v-list-tile avatar>
-              <v-list-tile-avatar>
-                <img src="https://randomuser.me/api/portraits/men/85.jpg" >
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title>John Leider</v-list-tile-title>
-              </v-list-tile-content>
-              <v-list-tile-action>
-                <v-btn icon @click.native.stop="mini = !mini">
-                  <v-icon>chevron_left</v-icon>
-                </v-btn>
-              </v-list-tile-action>
-            </v-list-tile>
-          </v-list>
-        </v-toolbar>
-        <v-list class="pt-0" dense>
-          <v-divider></v-divider>
-          <v-list-tile v-for="item in items" :key="item.title" @click="">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
-      <v-list class="grow">
-        <v-list-tile
-          v-for="link in links"
-          :key="link"
-          @click=""
-        >
-          <v-list-tile-title v-text="link"></v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-layout>
-	</v-navigation-drawer>
+	<div style="width: 100%; height: 100%;">
+		<v-navigation-drawer v-model="drawer" class="pb-0" floating hide-overlay stateless absolute="true" app>
+			<v-layout fill-height>
+				<v-navigation-drawer
+				dark
+				mini-variant
+				stateless
+				value="true"
+				>
+				<v-toolbar flat class="transparent">
+					<v-list class="pa-0">
+						<v-list-tile avatar>
+							<v-list-tile-avatar>
+								<img src="https://randomuser.me/api/portraits/men/85.jpg" >
+							</v-list-tile-avatar>
+							<v-list-tile-content>
+								<v-list-tile-title>John Leider</v-list-tile-title>
+							</v-list-tile-content>
+							<v-list-tile-action>
+								<v-btn icon @click.native.stop="mini = !mini">
+									<v-icon>chevron_left</v-icon>
+								</v-btn>
+							</v-list-tile-action>
+						</v-list-tile>
+					</v-list>
+				</v-toolbar>
+				<v-list class="pt-0" dense>
+					<v-divider></v-divider>
+					<v-list-tile v-for="item in items" :key="item.title" @click="">
+						<v-list-tile-action>
+							<v-icon>{{ item.icon }}</v-icon>
+						</v-list-tile-action>
+						<v-list-tile-content>
+							<v-list-tile-title>{{ item.title }}</v-list-tile-title>
+						</v-list-tile-content>
+					</v-list-tile>
+				</v-list>
+				</v-navigation-drawer>
+				<v-list class="grow">
+					<v-list-tile
+					v-for="link in links"
+					:key="link"
+					@click=""
+					>
+						<v-list-tile-title v-text="link"></v-list-tile-title>
+					</v-list-tile>
+				</v-list>
+			</v-layout>
+		</v-navigation-drawer>
+		<v-content style="width: 100%; height: 100%; padding-left: 0;" >
+			<div fill-height>Testing</div>
+			<!--<v-card height="200px">-->
+				<v-bottom-nav :value="true" :active.sync="e2" :color="color" absolute shift>
+				</v-bottom-nav>
+			<!--</v-card>-->
+		</v-content>
+	</div>
 </template>
 
 <script type="text/javascript">
@@ -56,13 +65,24 @@
 			return {
 				drawer: true,
 				items: [
-					{ title: 'Home', icon: 'dashboard' },
-					{ title: "About", icon: 'question_answer' },
+				{ title: 'Home', icon: 'dashboard' },
+				{ title: "About", icon: 'question_answer' },
 				],
 				links: ['Home', 'Contacts', 'Settings'],
 				mini: true,
-				right: null
+				right: null,
+				e2: 3
 			};
+		},
+		computed: {
+			color() {
+				switch (this.e2) {
+					case 0: return 'blue-grey'
+					case 1: return 'teal'
+					case 2: return 'brown'
+					case 3: return 'indigo'
+				}
+			}
 		}
 	};
 </script>

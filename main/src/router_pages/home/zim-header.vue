0c2841de38365f3d223d09864b1b4c716233afd7
Ivanq <imachug@yandex.ru>
2018-05-26 13:54:00 +0300

    Mobile header

--- main/src/router_pages/home/zim-header.vue
+++ main/src/router_pages/home/zim-header.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="header">
-		ZeroNet Instant Messenger
+		<span class="big">ZeroNet Instant Messenger</span>
+		<span class="small">ZiM</span>
 	</div>
 </template>
 
@@ -10,6 +11,20 @@
 		padding: 26px 16px
 
 		border-bottom: 1px solid darken($light-color, 5%)
+
+	.small
+		display: none
+
+		text-align: center
+		font-size: 24px
+		font-style: italic
+		letter-spacing: 4px
+
+	@include apply-to(less-than, $phone)
+		.big
+			display: none
+		.small
+			display: block
 </style>
 
 <script type="text/javascript">

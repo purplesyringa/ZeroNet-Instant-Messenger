bca84523df186e31cd3fcdcabd233d23e882da81
Ivanq <imachug@yandex.ru>
2018-05-27 10:17:22 +0300

    Remove old vuetify code

--- main/src/router_pages/channels/zim-message-list.vue
+++ main/src/router_pages/channels/zim-message-list.vue
@@ -13,26 +13,6 @@
 			<div class="clearfix" />
 		</template>
 	</div>
-
-	<!--
-	<v-content style="padding-left: 0; padding-right: 0; padding-top: 0;" class="fill-height">
-		<v-card class="fill-height" style="height: 100%;" flat>
-			<div style="padding: 10px; padding-left: 15px; padding-right: 15px; overflow-y: scroll; height: 100%; width: 100%; position: absolute; padding-bottom: 50px;">
-				<div v-for="(message, index) in messages" :style="index + 1 > messages.length - 1 || messages[index + 1].username != message.username ? 'margin-bottom: 7px;' : 'margin-bottom: 0;'">
-					<strong style="font-size: 1.2em;" v-if="(index > 0 &&messages[index - 1].username != message.username) || index == 0">{{ message.username }}</strong>
-					<div>{{ message.text }}</div>
-				</div>
-			</div>
-			<div class="elevation-2" style="position: absolute; bottom: 0; width: 100%; height: 50px; background: #424242; color: white; padding: 3px;">
-				<v-text-field
-					name="Message"
-					value="Testing"
-					style="margin: 0; color: white;" color="white"
-				/>
-			</div>
-		</v-card>
-	</v-content>
-	-->
 </template>
 
 <style lang="sass" scoped>

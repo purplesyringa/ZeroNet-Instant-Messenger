fca9bc15c5a873f5a9c1fec192216f1eb6925d1d
Ivanq <imachug@yandex.ru>
2018-05-27 19:32:10 +0300

    Schedule

--- main/src/libs/user/scheduled-storage.js
+++ main/src/libs/user/scheduled-storage.js
@@ -1,10 +1,14 @@
-import {zeroFS, zeroAuth, zeroPage} from "route.js";
+import {zeroFS, zeroPage} from "route.js";
 
 export default class ScheduledStorage {
 	constructor(path, contentJson) {
 		this.path = path;
 		this.content = contentJson;
 		this.cached = {};
+
+		this.lastPublish = 0;
+		this.runningTimeout = null;
+		this.delay = 30000;
 	}
 
 	async read(name) {
@@ -20,6 +24,24 @@ export default class ScheduledStorage {
 
 		let json = JSON.stringify(value);
 		await zeroFS.writeFile(`${this.path}/${name}.json`, json);
-		await zeroPage.publish(this.contentJson);
+
+		if(Date.now() - this.lastPublish > this.delay) {
+			this.lastPublish = Date.now();
+			zeroPage.publish(this.contentJson);
+		} else {
+			if(this.runningTimeout === null) {
+				// Already scheduled
+				return;
+			}
+
+			this.schedule();
+		}
+	}
+
+	schedule() {
+		this.runningTimeout = setTimeout(() => {
+			this.lastPublish = Date.now();
+			zeroPage.publish(this.contentJson);
+		}, this.delay);
 	}
 }

34fc4daf3595ebe9d3fc6c629ff0ec2d46130b21
Ivanq <imachug@yandex.ru>
2018-05-27 10:14:28 +0300

    Add login page

--- main/src/vue_components/root.vue
+++ main/src/vue_components/root.vue
@@ -1,20 +1,52 @@
 <template>
-	<div class="current-view" style="width: 100%; height: 100%;">
-		<router-page>
-			<component :is="$parent.mainView" slot="main-view" />
-			<component :is="$parent.pagesView" />
-		</router-page>
+	<div style="width: 100%; height: 100%;">
+		<login-page :class="{hidden: siteInfo.cert_user_id}" />
+		<div class="current-view" style="width: 100%; height: 100%;" v-if="siteInfo.cert_user_id">
+			<router-page>
+				<component :is="$parent.mainView" slot="main-view" />
+				<component :is="$parent.pagesView" />
+			</router-page>
+		</div>
 	</div>
 </template>
 
+<style lang="sass" scoped>
+	.hidden
+		position: absolute
+
+		opacity: 0 !important // sorry
+</style>
+
 <script language="text/javascript">
 	import RouterPage from "./router-page.vue";
+	import LoginPage from "router_pages/login-page/login-page.vue";
 
 	export default {
 		props: [],
 		name: "root",
+		data() {
+			return {
+				siteInfo: {
+					cert_user_id: ""
+				}
+			};
+		},
+		mounted() {
+			this.$eventBus.$on("setSiteInfo", this.onSiteInfo);
+		},
+		destroyed() {
+			this.$eventBus.$off("setSiteInfo", this.onSiteInfo);
+		},
+
+		methods: {
+			onSiteInfo(siteInfo) {
+				this.siteInfo = siteInfo;
+			}
+		},
+
 		components: {
-			"router-page": RouterPage
+			"router-page": RouterPage,
+			"login-page": LoginPage
 		}
 	};
 </script>

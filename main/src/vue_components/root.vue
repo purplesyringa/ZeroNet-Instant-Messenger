36c1cd1f775a84b70cedfc5f57be96e0ebca9d63
Ivanq <imachug@yandex.ru>
2018-05-27 15:24:23 +0300

    Register page

--- main/src/vue_components/root.vue
+++ main/src/vue_components/root.vue
@@ -1,7 +1,9 @@
 <template>
 	<div style="width: 100%; height: 100%;">
-		<login-page :class="{hidden: siteInfo.cert_user_id}" />
-		<div class="current-view" style="width: 100%; height: 100%;" v-if="siteInfo.cert_user_id">
+		<login-page :class="{hidden: (siteInfo.cert_user_id && user) || registerPage}" />
+		<register-page v-if="registerPage" />
+
+		<div class="current-view" style="width: 100%; height: 100%;" v-if="siteInfo.cert_user_id && user">
 			<router-page>
 				<component :is="$parent.mainView" slot="main-view" />
 				<component :is="$parent.pagesView" />
@@ -19,35 +21,70 @@
 </style>
 
 <script language="text/javascript">
+	import Vue from "vue/dist/vue.min.js";
+	import UserManager from "libs/user/manager";
 	import RouterPage from "./router-page.vue";
 	import LoginPage from "router_pages/login-page/login-page.vue";
+	import RegisterPage from "router_pages/login-page/register-page.vue";
 
 	export default {
 		props: [],
 		name: "root",
 		data() {
 			return {
+				registerPage: false,
 				siteInfo: {
 					cert_user_id: ""
-				}
+				},
+				user: null
 			};
+		},
+
+		beforeCreate() {
+			Vue.prototype.$user = null;
 		},
 		mounted() {
 			this.$eventBus.$on("setSiteInfo", this.onSiteInfo);
+			this.$eventBus.$on("registered", this.onRegistered);
 		},
 		destroyed() {
 			this.$eventBus.$off("setSiteInfo", this.onSiteInfo);
+			this.$eventBus.$off("registered", this.onRegistered);
 		},
 
 		methods: {
-			onSiteInfo(siteInfo) {
+			async onSiteInfo(siteInfo) {
 				this.siteInfo = siteInfo;
+
+				if(!siteInfo.cert_user_id) {
+					this.registerPage = false;
+					return;
+				}
+
+				if(Vue.prototype.$user) {
+					return;
+				}
+
+				if(await UserManager.isRegisteredSelf()) {
+					Vue.prototype.$user = UserManager.getSelf();
+					this.user = Vue.prototype.$user;
+					console.log("SET");
+				} else {
+					this.registerPage = true;
+				}
+			},
+
+			onRegistered() {
+				this.registerPage = false;
+				Vue.prototype.$user = UserManager.getSelf();
+				this.user = Vue.prototype.$user;
 			}
 		},
 
 		components: {
 			"router-page": RouterPage,
-			"login-page": LoginPage
+			"login-page": LoginPage,
+			"register-page": RegisterPage
 		}
 	};
 </script>

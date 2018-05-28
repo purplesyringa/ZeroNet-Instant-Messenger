89fc0d30e910c46d98c66e476cba2eadf89ebcb1
Ivanq <imachug@yandex.ru>
2018-05-28 10:50:57 +0300

    Emit recvBroadcastMessage event

--- main/src/libs/channel/p2p-messenger.js
+++ main/src/libs/channel/p2p-messenger.js
@@ -20,13 +20,16 @@ export default class P2PMessenger extends EventEmitter {
 		// Reemit big events into small ones
 		this.on("recvBroadcast", message => {
 			if(message.message.to) {
-				this.emit("recvBroadcastMessage:" + message.message.to, {
+				const evt = {
 					to: message.message.to,
 					authId: message.signed_by,
 					userName: message.cert.split("/")[1],
 
 					content: message.message.content
-				});
+				};
+
+				this.emit("recvBroadcastMessage:" + message.message.to, evt);
+				this.emit("recvBroadcastMessage", evt);
 			}
 		});
 	}
